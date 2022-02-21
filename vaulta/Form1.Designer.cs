0d94b755867dfe282d7b80614b1e620b70b5c4cf
lukachkhapelia <rusa@DESKTOP-ICTOHVV>
2022-02-21 19:49:30 +0400

    Add project files.

---
+++ vaulta/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace vaulta
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtoutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GEL",
+            "USD",
+            "EUR",
+            "YEN"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 91);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(81, 43);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "GEL",
+            "USD",
+            "EUR",
+            "YEN"});
+            this.comboBox2.Location = new System.Drawing.Point(258, 92);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(76, 43);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // txtoutput
+            // 
+            this.txtoutput.Font = new System.Drawing.Font("Segoe UI", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.txtoutput.Location = new System.Drawing.Point(365, 93);
+            this.txtoutput.Name = "txtoutput";
+            this.txtoutput.Size = new System.Drawing.Size(95, 41);
+            this.txtoutput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "curreny exchange";
+            // 
+            // txtinput
+            // 
+            this.txtinput.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtinput.Location = new System.Drawing.Point(128, 92);
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(98, 41);
+            this.txtinput.TabIndex = 4;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(140, 149);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(119, 45);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "calculate";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::vaulta.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(302, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(481, 206);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtinput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtoutput);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtoutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtinput;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ vaulta/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaulta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string a, b;
+        int c;
+      
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            a = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            b =  this.comboBox2.GetItemText(this.comboBox2.SelectedItem);
+        }
+
+       
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            c = int.Parse(txtinput.Text);
+            if (a == "GEL" && b == "GEL")
+            {
+                txtoutput.Text = c.ToString();
+            }
+            if (a == "GEL" && b == "USD")
+            {
+                txtoutput.Text = (c / 3.14).ToString();
+            }
+            if (a == "GEL" && b == "EUR")
+            {
+                txtoutput.Text = (c / 4).ToString();
+            }
+            if (a == "GEL" && b == "YAN")
+            {
+                txtoutput.Text = (c * 38).ToString();
+            }
+
+
+
+
+            if (a == "USD" && b == "GEL")
+            {
+                txtoutput.Text = (c * 3.14).ToString();
+            }
+            if (a == "USD" && b == "USD")
+            {
+                txtoutput.Text = c.ToString();
+            }
+            if (a == "USD" && b == "EUR")
+            {
+                txtoutput.Text = (c * 0.88).ToString();
+            }
+            if (a == "USD" && b == "YAN")
+            {
+                txtoutput.Text = (c * 144).ToString();
+            }
+
+
+
+            if (a == "EUR" && b == "GEL")
+            {
+                txtoutput.Text = (c * 4).ToString();
+            }
+            if (a == "EUR" && b == "USD")
+            {
+                txtoutput.Text = (c * 1.14).ToString();
+            }
+            if (a == "EUR" && b == "EUR")
+            {
+                txtoutput.Text = c.ToString();
+            }
+            if (a == "EUR" && b == "YAN")
+            {
+                txtoutput.Text = (c * 130).ToString();
+            }
+
+
+            if (a == "YAN" && b == "GEL")
+            {
+                txtoutput.Text = (c * 0.026).ToString();
+            }
+            if (a == "YAN" && b == "USD")
+            {
+                txtoutput.Text = (c * 0.0087).ToString();
+            }
+            if (a == "YAN" && b == "EUR")
+            {
+                txtoutput.Text = (c * 0.0077).ToString();
+            }
+            if (a == "YAN" && b == "YAN")
+            {
+                txtoutput.Text = c.ToString();
+            }
+        }
+
+     
+    }
+        
+}
